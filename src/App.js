2a0fca7de20d0aba927eb9f659b0f84de7d948df
iammostafa76 <iamamirmostafa@gmail.com>
2021-10-21 11:49:47 +0600

    added square

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Square from './components/Square';
+import { Pattern } from './components/Pattern';
 
 function App() {
+
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("X")
+
+  const chooseSquare = (square) => {
+    console.log(square)
+    setBoard(board.map((val, idx) => {
+      if(idx === square && val === ""){
+        return player;
+      }
+
+      return val
+    }))
+
+    if(player === "X"){
+      setPlayer("0")
+    }else {
+      setPlayer("X")
+    }
+
+
+  };
+
+  const checkWin = () => {
+    Pattern.forEach((currentPattern) => {
+      const firstPlayer = board[currentPattern[0]];
+
+      currentPattern.forEach((idx) => {
+        
+      })
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="board">
+        <div className="row">
+          <Square val={board[0]} chooseSquare={() => chooseSquare(0)}/>
+          <Square val={board[1]} chooseSquare={() => chooseSquare(1)}/>
+          <Square val={board[2]} chooseSquare={() => chooseSquare(2)}/>
+        </div>
+        <div className="row">
+          <Square val={board[3]} chooseSquare={() => chooseSquare(3)}/>
+          <Square val={board[4]} chooseSquare={() => chooseSquare(4)}/>
+          <Square val={board[5]} chooseSquare={() => chooseSquare(5)}/>
+        </div>
+        <div className="row">
+          <Square val={board[6]} chooseSquare={() => chooseSquare(6)}/>
+          <Square val={board[7]} chooseSquare={() => chooseSquare(7)}/>
+          <Square val={board[8]} chooseSquare={() => chooseSquare(8)}/>
+        </div>
+      </div>
     </div>
   );
 }
